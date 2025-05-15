6483996e48fce5a3df0fe7a372ce8aece11da00a
Michelangelo Morais <mickeey.m.200@gmail.com>
2025-05-15 12:58:24 -0300

    Tela inicio criada e trilha sonora adicionada em tela de inicio, in game e game over

--- src/game.c
+++ src/game.c
@@ -3,6 +3,9 @@
 #include <math.h>
 #include <windows.h>
 #include <stdbool.h>
+#include <mmsystem.h>
+
+#pragma comment(lib, "winmm.lib")
 
 int nScreenWidth = 160;
 int nScreenHeight = 40;
@@ -92,22 +95,80 @@ char map[] =
 
     GameState currentState = STATE_MENU;
 
-    void drawMenu(wchar_t *screen) {
-    for (int i = 0; i < nScreenWidth * nScreenHeight; i++)
-        screen[i] = L' ';
 
-    const wchar_t *title = L"=== MEU JOGO ===";
+void drawMenu(wchar_t *screen) {
+    const wchar_t *asciiTitle[] = {
+        L":::::::.   :::         ...     :::::::. :: .::::::.   :::::::-.   ...    ::::::.    :::.  .,-:::::/ .,::::::     ...   :::.    :::.",
+        L" ;;;'';;'  ;;;      .;;;;;;;.   ;;;'';;',';;;`    `    ;;,   `';, ;;     ;;;`;;;;,  `;;;,;;-'````'  ;;;;''''  .;;;;;;;.`;;;;,  `;;;",
+        L" [[[__[[\\. [[[     ,[[     \\[[, [[[__[[\\. '[==/[[[[,   `[[     [[[['     [[[  [[[[[. '[[[[[   [[[[[[/[[cccc  ,[[     \\[[,[[[[[. '[[",
+        L" $$\"\"\"\"Y$$ $$'     $$$,     $$$ $$\"\"\"\"Y$$   '''    $    $$,    $$$$      $$$  $$$ \"Y$c$$\"$$c.    \"$\" $$\"\"\"\"  $$$,     $$$$$$ \"Y$c$$",
+        L"_88o,,od8Po88oo,.__\"888,_ _,88P_88o,,od8P  88b    dP    888_,o8P'88    .d888  888    Y88 `Y8bo,,,o88o888oo,__\"888,_ _,88P888    Y88",
+        L"\"\"YUMMMP\" \"\"\"\"YUMMM  \"YMMMMMP\" \"\"YUMMMP\"    \"YMmMY\"     MMMMP\"`   \"YmmMMMM\"\"  MMM     YM   `'YMUP\"YMM\"\"\"\"YUMMM \"YMMMMMP\" MMM     YM"
+    };
+
+    int titleArtHeight = sizeof(asciiTitle) / sizeof(asciiTitle[0]);
+    int titleArtStartY = 2;
+    for (int i = 0; i < titleArtHeight; i++) {
+        int artLen = wcslen(asciiTitle[i]);
+        int startX = (nScreenWidth - artLen) / 2;
+        swprintf(&screen[(titleArtStartY + i) * nScreenWidth + startX], L"%ls", asciiTitle[i]);
+    }
+
     const wchar_t *option1 = L"1. Iniciar Jogo";
     const wchar_t *option2 = L"2. Sair";
+    int optionStartY = titleArtStartY + titleArtHeight + 1;
+    int optionStartX = (nScreenWidth - wcslen(option1)) / 2;
 
-    int startY = nScreenHeight / 2 - 2;
-    int startX = (nScreenWidth - wcslen(title)) / 2;
+    swprintf(&screen[optionStartY * nScreenWidth + optionStartX], L"%ls", option1);
+    swprintf(&screen[(optionStartY + 1) * nScreenWidth + optionStartX], L"%ls", option2);
 
-    swprintf(&screen[startY * nScreenWidth + startX], L"%ls", title);
-    swprintf(&screen[(startY + 1) * nScreenWidth + startX], L"%ls", option1);
-    swprintf(&screen[(startY + 2) * nScreenWidth + startX], L"%ls", option2);
-
+    const wchar_t *artImage[] = {
+        L"                ..##################@@####MM..              ",
+        L"            @@####@@@@@@@@##########@@@@@@@@####@@          ",
+        L"          ##@@@@MMmmmm@@@@@@######@@@@@@mmmm@@@@@@MM        ",
+        L"        ++@@@@@@mmmmmmmm@@@@@@##@@@@@@mmmmmmmm@@@@##        ",
+        L"      ..##@@MMmmMMMMmmmmMM@@@@@@@@@@MMmmmmMMMMmmMM@@##      ",
+        L"      ##@@@@mmmmMM@@MMmmmmMM@@@@@@MMmmmmMM@@MMmmmm@@####    ",
+        L"    ####@@@@mmmmMM@@@@@@mmMM@@@@@@MMMM@@@@@@MMmmmm@@######  ",
+        L"    ####@@@@@@mmmmmmmmmmMM@@@@@@@@@@mmmmmmmmmmmm@@@@######  ",
+        L"  ########@@@@MMMMmmmmMM@@@@@@##@@@@@@MMmmmmMM@@@@@@######@@",
+        L"  ##########@@@@MMMMMM@@@@@@######@@@@@@MMMM@@@@@@##########",
+        L"  ################@@@@@@##############@@@@@@################",
+        L"  ##########################################################",
+        L"  ##########################################################",
+        L"  MM######################################################++",
+        L"    ######################################################  ",
+        L"      ##################################################    ",
+        L"        ##############################################      ",
+        L"          @@######################################++        "
+    };
+    int artImageHeight = sizeof(artImage) / sizeof(artImage[0]);
+    int artImageStartY = optionStartY + 4;
+    for (int i = 0; i < artImageHeight; i++) {
+        int lineLen = wcslen(artImage[i]);
+        int startX = (nScreenWidth - lineLen) / 2;
+        swprintf(&screen[(artImageStartY + i) * nScreenWidth + startX], L"%ls", artImage[i]);
     }
+
+    const wchar_t *fireArt[] = {
+        L"            --                              MM::++mmmmMM      MMmmmm            ..mm++++....++  --  --..                        ++++MM      ++++--  ..  ....                mm                          ",
+        L"            ++mm        ::::mm            mm++mm++++++::mm++MM++MMmm    ::        ..mm::::..++::--++..      --    ::      --..--  --::mm  ----......  ......                ::++::----    ::           ",
+        L"            --          mm::        mmmmmm    MMmmMM--mm@@@@mmMM++MM--++MMmm----mm@@MMMM@@MMmm--++++::mm::----....mm--....@@mmMM::mm..--::++mm::::--..mm----..mmmmMM@@MMmm++          ::--              ",
+        L"          ::..            ++        MMmmmm    MM@@++MMmm++MMmmmm++::--::::mm::--::MM++mm####@@MMMMmm--::::..------..::mm::MM@@mm..::....::--::..--::::..  --    ++@@mmMMmm--          --                ",
+        L"        ++....            ::    ::    ::::  mmmmmm  MMMMmm++++--::----::--::--::--..++MM@@@@@@MMMM++--::..--  --mm  ----MM@@MM--::::::MM++..--....--mmmm..::  mmmmMMMM::++..--::      ::..              ",
+        L"        ..              mm::++  ++    ++++@@++::++++@@@@mm--++++++::..::mm..  ::--..++MMmmMM@@++MM++--++......::--    mm##@@@@--++----MMmm..++  ..++++mm::++++--++mm@@mm----..++..    ::++++....        ",
+        L"        ++--::  --....----....mmmmmm--mm++++++mmmmmm----++----@@mm....--..::::      mmMMMMMMMM++mm++--::--::mm..  ..  MMmmMMMM..--++mm::++..++--..++mmMM::--++++++++MMMM--mm--::::++++::++++::....      ",
+        L"        mm++mm++MMmmMM++mmmmmmmm@@mmMM..++++mmmmMMmmMM::::mm::MMMMmm::++::++::--::::MMmmmm++mmmmmmmmMMMM++mmMM++mm++mm::++++++MM++++::mmmm++mm++mmMM@@MM##MMMMmm@@@@mm++++mm++::++--mmmm++@@mm++++      "
+    };
+
+    int fireArtHeight = sizeof(fireArt) / sizeof(fireArt[0]);
+    int fireStartY = nScreenHeight - fireArtHeight - 1;  // -1 pra não colar no rodapé
+    for (int i = 0; i < fireArtHeight; i++) {
+        int lineLen = wcslen(fireArt[i]);
+        int startX = (nScreenWidth - lineLen) / 2;
+        swprintf(&screen[(fireStartY + i) * nScreenWidth + startX], L"%ls", fireArt[i]);
+    }
+}
 
 int main() {
     wchar_t *screen = malloc(sizeof(wchar_t) * nScreenWidth * nScreenHeight);
@@ -123,6 +184,9 @@ int main() {
     DWORD dwBytesWritten = 0;
 
     bool enemyChasing = false;
+    bool menuMusicPlaying = false;
+    bool playMusicPlaying = false;
+    bool gameoverMusicPlaying = false;
 
     LARGE_INTEGER frequency, t1, t2;
     QueryPerformanceFrequency(&frequency);
@@ -136,6 +200,12 @@ int main() {
         switch (currentState)
         {
         case STATE_MENU:
+
+            if (!menuMusicPlaying) {
+                PlaySound(TEXT("assets/intro.wav"), NULL, SND_FILENAME | SND_ASYNC | SND_LOOP);
+                menuMusicPlaying = true;
+            }
+
             drawMenu(screen);
 
             for (int i = 0; i < nScreenWidth * nScreenHeight; i++) {
@@ -145,8 +215,10 @@ int main() {
             WriteConsoleOutputCharacterW(hConsole, screen, nScreenWidth * nScreenHeight, (COORD){0, 0}, &dwBytesWritten);
 
             if (GetAsyncKeyState('1') & 0x8000) {
-            currentState = STATE_PLAYING;
-            Sleep(200);
+                PlaySound(NULL, 0, 0); // Para a música
+                menuMusicPlaying = false;
+                currentState = STATE_PLAYING;
+                Sleep(200);
             } else if (GetAsyncKeyState('2') & 0x8000) {
                 free(screen);
                 free(colors);
@@ -155,6 +227,12 @@ int main() {
             break;
 
         case STATE_PLAYING:
+
+            if (!playMusicPlaying) {
+                PlaySound(TEXT("assets/play.wav"), NULL, SND_FILENAME | SND_ASYNC | SND_LOOP);
+                playMusicPlaying = true;
+            }    
+
             if (GetAsyncKeyState('A') & 0x8000)
                 fPlayerA -= (fSpeed * 0.75f) * fElapsedTime;
             if (GetAsyncKeyState('D') & 0x8000)
@@ -179,6 +257,15 @@ int main() {
             }
 
             if ((int)fPlayerX == (int)fEnemyX && (int)fPlayerY == (int)fEnemyY) {
+
+                PlaySound(NULL, 0, 0); // Para a música de jogo
+                playMusicPlaying = false;
+
+                if (!playMusicPlaying) {
+                PlaySound(TEXT("assets/gameover.wav"), NULL, SND_FILENAME | SND_ASYNC );
+                gameoverMusicPlaying = true;
+                } 
+
                 fPlayerX = 25.99f;
                 fPlayerY = 10.70f;
                 fEnemyX = 3.48f;
